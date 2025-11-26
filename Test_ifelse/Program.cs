9fc3d7023a5455a6621edf1727b90651daf38e4e
Leenlnza <niwat3706@gmail.com>
2025-11-26 09:08:56 +0700

    ifelse

---
+++ Test_ifelse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int age = 25;
+        if (age < 13)
+        {
+            Console.WriteLine("เด็ก");
+        }
+        else if (age < 20)
+        {
+            Console.WriteLine("วัยรุ่น");
+        }
+        else if (age < 60)
+        {
+            Console.WriteLine("ผู้ใหญ่");
+        }
+        else
+        {
+            Console.WriteLine("ผู้สูงอายุ");
+
+        }
+    }
+}
